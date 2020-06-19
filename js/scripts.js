6d39995a639bcee02f6ae6737bc4d94de0c38f8f
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-19 15:58:20 -0700

    add options for extra toppings

--- js/scripts.js
+++ js/scripts.js
@@ -25,18 +25,19 @@ Order.prototype.findPizza = function(id) {
   return false;
 }
 
-let pizzaOrder = new Order();
+// Bussiness Logic for Pizzas
 
-// Bussiness Logci for Pizzas
-
-
-function Pizza(size, top1,) {
+function Pizza(size, top1, top2, top3) {
   this.size = size;
   this.top1 = top1;
+  this.top2 = top2;
+  this.top3 = top3;
 }
 
 Pizza.prototype.pricer = function() {
   price = 0;
+
+//Size conditions
     if (this.size.includes("14")) {
         price += 12;
     } else if (this.size.includes("18")){
@@ -44,6 +45,7 @@ Pizza.prototype.pricer = function() {
     } else if (this.size.includes("24")){
         price += 20;
     } 
+//Top1 conditions
     if (this.top1.includes("Fresh")){
         price += 1;
     } else if (this.top1.includes("Cal")){
@@ -53,7 +55,26 @@ Pizza.prototype.pricer = function() {
     } else if (this.top1.includes("Squ")){
         price += 6;
     }
-    
+//Top2 conditions   
+    if (this.top2.includes("Fresh")){
+      price += 1;
+  } else if (this.top2.includes("Cal")){
+      price += 2;
+  } else if (this.top2.includes("Pep")){
+      price += 2;
+  } else if (this.top2.includes("Squ")){
+      price += 6;
+  }
+//Top3 conditions   
+  if (this.top3.includes("Fresh")){
+    price += 1;
+} else if (this.top3.includes("Cal")){
+    price += 2;
+} else if (this.top3.includes("Pep")){
+    price += 2;
+} else if (this.top3.includes("Squ")){
+    price += 6;
+}
     console.log(price)
     return price;
 }
@@ -62,17 +83,20 @@ Pizza.prototype.pricer = function() {
 $(document).ready(function() {
   $("form#new-pizza").submit(function(event) {
     event.preventDefault();
+    let pizzaOrder = new Order();
     const size = $("#size").val();
     const top1 = $("#top1Choice").val();
-    let newPizza = new Pizza (size, top1);
+    const top2 = $("#top2Choice").val();
+    const top3 = $("#top3Choice").val();
+    let newPizza = new Pizza (size, top1, top2, top3);
     pizzaOrder.addPizza(newPizza);
     //$(".total").text(pricer(size, top1));
     let price = newPizza.pricer();
     $(".total").html("Your total is:" + " " + "$" + price);
     $(".pizzaSize").html(newPizza.size);
-    $(".pizzaTop1").html(newPizza.top1);
     $(".top1").html(newPizza.top1);
-    
+    $(".top2").html(newPizza.top2);
+    $(".top3").html(newPizza.top3);
     console.log(price);
   })
 })
